b19984976f3b5634fb9b7f9e0aa80c965678b8eb
adw2s <hancw9508@gmail.com>
2021-03-16 20:55:56 +0800

    Update 11fqkk.js

--- Task/11fqkk.js
+++ Task/11fqkk.js
@@ -70,7 +70,7 @@ hostname = m.*
 
 const $ = new Env('番茄看看');
 const fqkkurlArr = [], fqkkhdArr = []
-let fqkk = $.getjson( process.env.fqkkhd, [])
+let fqkk = process.env.fqkkhd
 let fqkkBanfirstTask = $.getval('fqkkBanfirstTask') || 'false' // 禁止脚本执行首个任务，避免每日脚本跑首次任务导致微信限制
 let fqkkCkMoveFlag = $.getval('fqkkCkMove') || ''
 let fqtx = ($.getval('fqtx') || '100');  // 此处修改提现金额，0.3元等于30，默认为提现一元，也就是100
